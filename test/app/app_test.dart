085a1321f9a4c87f45461345bdeb69283ae0bb9a
Daniel Górka <daniel.gorka24@gmail.com>
2021-05-25 11:40:02 +0200

    Create simple app

---
+++ test/app/app_test.dart
@@ -0,0 +1,12 @@
+import 'package:clothes/app/app.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('App', () {
+    testWidgets('should app starts', (tester) async {
+      await tester.pumpWidget(App());
+      expect(find.text('Hello world!'), findsOneWidget);
+    });
+
+  });
+}
