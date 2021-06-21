b10231b1d541b439551224d1245a56fd9c283de9
Daniel Górka <daniel.gorka24@gmail.com>
2021-06-21 16:38:30 +0200

    Update app theme and add SystemUIOverlayStyles

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,8 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:clothes/app/routes/router.gr.dart';
+import 'package:clothes/app/utils/keys.dart';
 import 'package:clothes/app/utils/theme.dart';
 import 'package:clothes/l10n/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class App extends StatefulWidget {
@@ -23,6 +25,32 @@ class _AppState extends State<App> {
       darkTheme: AppTheme.dark,
       routeInformationParser: _router.defaultRouteParser(),
       routerDelegate: AutoRouterDelegate(_router),
+      builder: (context, child) {
+        return SystemUiOverlayRegion(
+          child: child!,
+        );
+      },
+    );
+  }
+}
+
+@visibleForTesting
+class SystemUiOverlayRegion extends StatelessWidget {
+  final Widget child;
+
+  const SystemUiOverlayRegion({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      key: Keys.systemUiOverlayAnnotatedRegion,
+      value: Theme.of(context).brightness == Brightness.dark
+          ? AppTheme.overlayDark
+          : AppTheme.overlayLight,
+      child: child,
     );
   }
 }
