cee033096d979975c3b5ed79dd967f2dfe5f0dff
Daniel Górka <daniel.gorka24@gmail.com>
2021-07-07 15:38:39 +0200

    Add findSystemLocale() to main.dart

--- lib/app/configure/configure_web.dart
+++ lib/app/configure/configure_web.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:intl/intl_browser.dart';
 
-void configureApp() {
+Future<void> configureApp() async {
   setUrlStrategy(PathUrlStrategy());
+  await findSystemLocale();
 }
